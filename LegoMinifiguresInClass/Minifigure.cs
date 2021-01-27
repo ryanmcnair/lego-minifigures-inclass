1bc1591ac0c92107ee3e70d44056ae7823c63be7
Ryan McNair <ryan.mcnair@gmail.com>
2021-01-26 21:01:21 -0600

    inclass code along

---
+++ LegoMinifiguresInClass/Minifigure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LegoMinifiguresInClass.Pieces.Heads;
+using LegoMinifiguresInClass.Pieces.Torsos;
+using LegoMinifiguresInClass.Pieces.Legs;
+
+namespace LegoMinifiguresInClass
+{
+    class Minifigure
+    {
+        public Head Head { get; }
+        public Torso Torso { get; }
+        public LegsBase Legs { get; }
+        public string Name { get;  }
+        public Minifigure(Head head, Torso torso, LegsBase legs, string name)
+        {
+            Head = head;
+            Torso = torso;
+            Legs = legs;
+            Name = name;
+        }
+
+        public void MeetSomeone()
+        {
+            Console.WriteLine($"{Name} is starting the greeting process.");
+            Legs.Walk();
+            Torso.Wave();
+            Head.Greet();
+        }
+    }
+}
---
+++ LegoMinifiguresInClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using LegoMinifiguresInClass.Pieces;
+using LegoMinifiguresInClass.Pieces.Heads;
+using LegoMinifiguresInClass.Pieces.Torsos;
+using LegoMinifiguresInClass.Pieces.Legs;
+
+namespace LegoMinifiguresInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coolHead = new CoolHead();
+            coolHead.Eyewear = "Sunglasses";
+            coolHead.Hair = "Long";
+            coolHead.Gender = Gender.Female;
+
+            var nerdHead = new NerdHead();
+            nerdHead.Acne = true;
+            nerdHead.Hat = "Fedora";
+
+            var skull = new Skull { Color = Color.Red, Condition = Condition.Refurbished, EyeColor = Color.Green, Eyewear = "Aviators" };
+            
+
+            var heads = new Head[] { coolHead, nerdHead, skull };
+
+            foreach (var head in heads)
+            {
+                
+                head.Talk("Stuff");
+            }
+
+            var pirateWithHook = new PirateTorso(true) { Clothing = Clothing.Hoodie };
+            var pirateWithoutHook = new PirateTorso(false) { Clothing = Clothing.Tanktop};
+
+            pirateWithHook.Wave();
+            pirateWithoutHook.Wave();
+
+            var torso = new Torso { Clothing = Clothing.Hoodie, Color = Color.Green, NumberOfArms = 0 };
+
+            var skeletonTorso = new SkeletonTorso(false, Condition.Broken);
+            var policeTorso = new PoliceOfficerTorso();
+
+            var torsos = new Torso[] { pirateWithHook, pirateWithoutHook, skeletonTorso, torso, policeTorso };
+
+            foreach (var body in torsos)
+            {
+                body.Wave();
+            }
+
+            var legs = new DiscoLegs();
+
+            var skullPoliceSpider = new Minifigure(skull, policeTorso, new SpiderLegs());
+            skullPoliceSpider.MeetSomeone();
+        }
+    }
+}
